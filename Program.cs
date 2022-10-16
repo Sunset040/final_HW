54cc1033be7bda5bbc46403c31e0da3ffbc5af9e
Aida <khabdreshova@mail.ru>
2022-10-16 16:05:56 +0300

    Попытка номер 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
 ﻿// Код проекта
+string[] arString = {"Mouse", "Cat", "Dog","Pie","pineapple", "tea"};
+int count = arString.Length;
+
+string[] arStringLite = new string[] {};
+
+for (int i = 0; i < count; i++)
+{
+    if (arString[i].Length <= 3){
+        Console.WriteLine(arString[i]);
+    }
+}
+Console.WriteLine("--------------------------------");
+Console.WriteLine("Result: ");
+for (int i = 0; i < arStringLite.Length; i++) {
+    Console.Write($"{arStringLite[i]}, ");
+}
